b6ed0e08a3447dc63ccbda06d05577defc704918
Nazar Brych <nazar.brych.06@gmail.com>
2023-02-15 12:11:42 +0200

    Configured Startup class

--- OnlineCatalog/OnlineCatalog/Startup.cs
+++ OnlineCatalog/OnlineCatalog/Startup.cs
@@ -1,11 +1,19 @@
+using AutoMapper;
+using BLL;
+using BLL.Interfaces;
+using BLL.Services;
+using DAL.Data;
+using DAL.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,18 +33,38 @@ namespace OnlineCatalog
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connectionString = Configuration.GetConnectionString("Catalog");
             services.AddControllers();
+            services.AddDbContext<ProjectDbContext>(builder =>
+                builder.UseSqlServer(connectionString)
+            );
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(new AutoMapperProfile()));
+            services.AddSingleton<IMapper>(im => new Mapper(configuration));
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IWorkerService, WorkerService>();
+            services.AddScoped<ILeaderService, LeaderService>();
+            services.AddScoped<ISubordinateService, SubordinateService>();
+            services.AddSwaggerGen(c =>
+               c.SwaggerDoc("v1", new OpenApiInfo { Title = "Online Catalog", Version = "v1" })
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+               c.SwaggerEndpoint("/swagger/v1/swagger.json", "Online Catalog Api V1")
+            );
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
             app.UseHttpsRedirection();
+
+            app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
             app.UseRouting();
 
