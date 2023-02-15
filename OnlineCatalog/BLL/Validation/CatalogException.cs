f0f2ff259fc64ffc5c0f595ffd40d01710cefa59
Nazar Brych <nazar.brych.06@gmail.com>
2023-02-15 04:28:23 +0200

    Adde Exception class for data validation

---
+++ OnlineCatalog/BLL/Validation/CatalogException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace BLL.Validation
+{
+    [Serializable]
+    public class CatalogException : Exception
+    {
+        public CatalogException()
+        {
+
+        }
+        public CatalogException(string message) : base(message)
+        {
+
+        }
+        public CatalogException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+
+        }
+        public CatalogException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+
+        }
+    }
+}
